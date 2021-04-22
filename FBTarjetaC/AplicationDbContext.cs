45bf3bdfbf6d2e15390db7d3ca5dcb02c8b15d20
jonathancofone <66129531+jonathancofone@users.noreply.github.com>
2021-04-22 12:34:41 -0300

    Add files via upload

---
+++ FBTarjetaC/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FBTarjetaC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTarjetaC
+{
+    public class AplicationDbContext: DbContext
+    {
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ FBTarjetaC/Models/TarjetaCredito.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTarjetaC.Models
+{
+    public class TarjetaCredito
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Titular { get; set; }
+        public string NumeroTarjeta{ get; set; }
+
+        public string FechaExpiracion { get; set; }
+
+        public string CVV { get; set; }
+
+    }
+}
